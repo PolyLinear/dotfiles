aa31a5c6fc5d6f3ec553b9cb4bfb52f1ede25103
PolyLinear <bciesla@umass.edu>
2024-01-19 09:25:13 -0500

    cleanup

--- config/nvim/test.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <vector>
-int add(int a, int b) { return a + b; }
-int more_stuff(std::vector<int> a, int b) { return 10; }
-int main() {
-	more_stuff(std::vector<int>a , 10);
-}
-
