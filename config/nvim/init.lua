8f5e21bb87fe69eec28004783ce34d8b92b7dfc0
PolyLinear <bciesla@umass.edu>
2024-06-23 17:10:56 -0400

    sway,zathura,waybar, and nvim updates

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -26,4 +26,11 @@ vim.api.nvim_create_autocmd("BufEnter", {
 	end,
 })
 
+local autopairs = require("nvim-autopairs")
+local Rule = require("nvim-autopairs.rule")
+local cond = require("nvim-autopairs.conds")
+autopairs.add_rules {
+  Rule("<", ">"):with_pair(cond.before_regex("%a+")):with_move(function(opts) return opts.char == ">" end),
+}
+
 require("lsp_signature").setup(cfg)
